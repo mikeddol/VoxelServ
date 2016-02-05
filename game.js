f501ab6aa7c3f2ba2ca6ff545d7a06e350f60e7e
Mike D <akagetsu@gmail.com>
2016-02-05 20:21:54 +0000

    Add an object for the game and a user

---
+++ game.js
@@ -0,0 +1,13 @@
+module.exports = {
+	id: "",
+	user_manager: {
+		users: [],
+		online: 0
+	},
+	time: {
+		timeStarted: 0,
+		timeCurrent: 0,
+		timeEnd: 0
+	},
+	full: false
+};
---
+++ user.js
@@ -0,0 +1,11 @@
+module.exports = {
+	id: "",
+	color: "",
+	kills: 0,
+	deaths: 0,
+	pos: {
+		x: 0,
+		y: 0
+	},
+	speed: 0
+};
