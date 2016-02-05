f6b42470f97049bda2ba184873426be2dca208b0
Mike D <akagetsu@gmail.com>
2016-02-05 20:22:25 +0000

    Write some methods for handling users and game getting

--- server.js
+++ server.js
@@ -18,6 +18,36 @@
 		socket.on('disconnect', function() {
 			console.log('user disconnected');
 		});
+
+		function addUser(usr) {
+			console.log(usr);
+			if(!game.full) {
+				game.user_manager.users.push({
+					id: usr.id,
+					color: usr.color
+				});
+				updateOnline();
+			}
+		}
+		function removeUser(id) {
+			game.user_manager.users = game.user_manager.users.filter(function (user) {
+				if(user.id != id) {
+					return user;
+				}
+			});
+		}
+
+		function updateOnline() {
+			game.user_manager.online = game.user_manager.users.length;
+		}
+
+		function getGame() {
+			return game;
+		}
+
+		function getUsers() {
+			return game.user_manager.users;
+		}
 	});
 
 	http.listen(3000, function() {
