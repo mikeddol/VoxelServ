44751ef6af3e87a5e314f1826569f8a62d35d04d
Mike D <akagetsu@gmail.com>
2016-02-18 15:16:22 +0000

    Make the User a proper object with a constructor and module

--- user.js
+++ user.js
@@ -1,9 +1,18 @@
-module.exports = {
-	uuid: "",
-	socketid: "",
-	color: "",
-	kills: 0,
-	deaths: 0,
-	pos: null,
-	rot: null
+function User(data) {
+	this.uuid = data.uuid;
+	this.socketid = data.socketid;
+	this.color = data.color;
+	this.kills = data.kills || 0;
+	this.deaths = data.deaths || 0;
+	this.pos = data.pos || null;
+	this.rot = data.rot || null;
+}
+
+User.prototype.update = function update(data) {
+	this.kills = data.kills;
+	this.deaths = data.deaths;
+	this.pos = data.pos;
+	this.rot = data.rot;
 };
+
+module.exports = User;
