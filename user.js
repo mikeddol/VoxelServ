9d66552114bb29faae1a161419090482db586d30
Mike D <akagetsu@gmail.com>
2016-04-15 17:13:39 +0100

    Add state object and update

--- user.js
+++ user.js
@@ -6,15 +6,37 @@ function User(data) {
 	this.kills = data.kills || 0;
 	this.deaths = data.deaths || 0;
 	this.pos = data.pos || null;
-	this.rot = data.rot || null;
 	this.dead = false;
+	this.state = {
+		"fow": false,
+		"bck": false,
+		"lef": false,
+		"rig": false,
+		"jmp": false,
+		"atk": false,
+		"vew": {
+			"dx": 0,
+			"dy": 0
+		},
+		"cam": {
+			"fow": {
+				"x": 0,
+				"z": 0
+			},
+			"rig": {
+				"x": 0,
+				"z": 0
+			}
+		},
+		"flr": false
+	};
 }
 
 User.prototype.update = function update(data) {
 	this.kills = data.kills;
 	this.deaths = data.deaths;
+	this.state = data.state;
 	this.pos = data.pos;
-	this.rot = data.rot;
 };
 
 module.exports = User;
