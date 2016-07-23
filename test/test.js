8ec1909549277abf0d0180a8ab8dd172bcb455e6
Mike D <akagetsu@gmail.com>
2016-07-23 10:58:48 +0100

    Add more tests and jsformat

--- test/test.js
+++ test/test.js
@@ -1,11 +1,9 @@
-/* jshint esversion: 6 */
-
 var assert = require('chai').assert;
 
 describe('Game', function() {
 	var game = {};
 	beforeEach("reinitialise the game object", function() {
-		game = new (require('../game'))();
+		game = new(require('../game'))();
 	});
 
 	it('should start with 0 users online', function() {
@@ -34,9 +32,9 @@ describe('Game', function() {
 		});
 
 		it(`should return the uuid of the removed user if successful`,
-		 function() {
-			assert.isNotFalse(game.removeUser('testId'));
-		});
+			function() {
+				assert.isNotFalse(game.removeUser('testId'));
+			});
 
 		it('should return false if removing user is unsuccessful', function() {
 			assert.isFalse(game.removeUser("testId2"));
@@ -51,6 +49,73 @@ describe('Game', function() {
 	describe('#getGame()', function() {
 		it('should return the game object', function() {
 			assert.isOk(game.getGame());
+		});
+	});
+
+	describe('#getUsers()', function() {
+		it('should return an empty object if no users are connected',
+			function() {
+				assert.equal(0, Object.keys(game.getUsers()).length);
+			});
+
+		it('should return one object if one single user joined the game',
+			function() {
+				game.addUser('testId');
+				assert.equal(1, Object.keys(game.getUsers()).length);
+			});
+	});
+
+	describe('#getColour()', function() {
+		it('should return yellow if the user joining is first in the game',
+			function() {
+				assert.equal('yellow', game.getColour('testId'));
+			});
+
+		it('should return red if user joining is second in the game',
+			function() {
+				game.getColour('testId');
+				assert.equal('red', game.getColour('testId2'));
+			});
+
+		it('should an empty string if all colours are taken', function() {
+			for (var i = 0; i < 4; i++) {
+				game.getColour('testId' + i);
+			}
+			assert.equal("", game.getColour('testid5'));
+		});
+	});
+
+	describe('#killUser()', function() {
+		var user = {};
+		beforeEach('add a user to the game', function() {
+			user = game.addUser('testId');
+			game.killUser(user.uuid);
+		});
+
+		it('should set the user dead state to true', function() {
+			assert.isTrue(user.dead);
+		});
+
+		it('should increase the deaths count of the user', function() {
+			assert.equal(1, user.deaths);
+		});
+	});
+
+	describe('#respawnUser()', function() {
+		it('should  reset the dead state of the user to true',
+			function() {
+				var user = game.addUser('testId');
+				game.killUser(user.uuid);
+				game.respawnUser(user.uuid);
+				assert.isFalse(user.dead);
+			});
+	});
+
+	describe('#rewardUser()', function() {
+		it('should increase the kills count of the user', function() {
+			var user = game.addUser('testId');
+			game.rewardUser(user.uuid);
+			assert.equal(1, user.kills);
 		});
 	});
 });
